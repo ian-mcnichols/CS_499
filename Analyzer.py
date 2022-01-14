bc11a81c45982789716400d6761db5b767cbcd42
Ian McNichols <90727326+ian-mcnichols@users.noreply.github.com>
2022-01-14 12:48:32 -0600

    Changing rows/cols to ranges

--- Analyzer.py
+++ Analyzer.py
@@ -7,10 +7,10 @@ class Analyzer:
     def __init__(self, data_type):
         self.data_type = data_type
 
-    def set_rows(self, new_rows):
-        self.rows = new_rows
-    def set_cols(self, new_cols):
-        self.cols = new_cols
+    def set_rows(self, row_start, row_end):
+        self.rows = range(row_start, row_end)
+    def set_cols(self, col_start, col_end):
+        self.cols = range(col_start, col_end)
 
     def run_mean(self, data):
         return np.mean(data)
