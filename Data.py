53cc6347bf1e7be9dc1c1280524db671c7217b02
Ian McNichols <90727326+ian-mcnichols@users.noreply.github.com>
2022-01-17 12:04:47 -0600

    Can add data by function instead of file

--- Data.py
+++ Data.py
@@ -1,9 +1,10 @@
 import numpy as np
 
 
-class Data:
+class Data: # TODO: Find a better way to get the numerical data's rows/cols than try catch blocks
     def __init__(self, data_type):
         self.data_type = data_type
+        self.data = None
 
     def read_data_file(self, filepath):
         with open(filepath, "r") as freq_file:
@@ -40,9 +41,27 @@ class Data:
             self.data = data_array
         return
 
+    def add_data(self, new_data):
+        new_data = np.array(new_data)
+        if self.data is None:
+            self.data = new_data
+        else:
+            if new_data.shape[0] == self.data.shape[0]:
+                self.data = np.append(self.data, new_data, axis=1)
+            elif new_data.shape[1] == self.data.shape[1]:
+                self.data = np.append(self.data, new_data, axis=0)
+            else:
+                raise Exception("Data to append must match current data's shape on axis 0 or 1")
+
+
 
 if __name__ == '__main__':
     # Testing with a file
     my_data = Data("Frequency")
-    my_data.read_data_file("/mnt/alam01/slow02/imcnichols/Homework/CS 499/Test_Data/OrdinalDataTest.csv")
-    print(my_data.data)
+    my_data.add_data(np.random.rand(10, 10))
+    print(my_data.data.shape)
+    my_data.add_data(np.random.rand(10, 2))
+    print(my_data.data.shape)
+    my_data.add_data(np.random.rand(1, 12))
+    print(my_data.data.shape)
+    #my_data.read_data_file("/mnt/alam01/slow02/imcnichols/Homework/CS 499/Test_Data/OrdinalDataTest.csv")
