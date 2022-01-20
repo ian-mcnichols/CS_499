b3fe803c0a37da741a723a731738ce2f2495cc63
Ian McNichols <90727326+ian-mcnichols@users.noreply.github.com>
2022-01-20 15:37:01 -0600

    Reformatting to work more like a GUI

--- driver.py
+++ driver.py
@@ -2,7 +2,7 @@ from Data import Data
 from Analyzer import Analyzer
 
 
-def main(filepath, data_type):
+def main():
     available_functions = {"Frequency": [], # We need more info on the statistical functions.
                            "Ordinal": [], 
                            "Interval": []}
@@ -19,10 +19,15 @@ def main(filepath, data_type):
                         "corr_coeff": [],
                         "rank_sum": [],
                         "spearman_rank": []}
-    my_functions = available_functions[data_type]
-    my_data = Data(filepath=filepath, data_type=data_type)
-    my_analyzer = Analyzer(data_type)
-    print(my_data.data.shape)
+    while True:
+        enter_file = input("Would you like to add data by file? y/n:  ")
+        if enter_file.lower() == "y":
+            data_file = input("Enter data file:  ")
+            data_type = input("Enter data type:  ")
+            my_functions = available_functions[data_type]
+            my_data = Data(filepath=data_file, data_type=data_type)
+            my_analyzer = Analyzer(data_type)
+        function_to_run = input("Enter the function to run on this data: ")
     
     return
 
