447564c96c886f6cbaf707720cfc428d9e831862
Ian McNichols <90727326+ian-mcnichols@users.noreply.github.com>
2022-01-21 13:04:14 -0600

    Fixing bugs

--- driver.py
+++ driver.py
@@ -26,16 +26,19 @@ def main():
             data_file = input("Enter data file:  ")
             data_type = input("Enter data type:  ")
             my_functions = available_functions[data_type]
-            my_data = Data(filepath=data_file, data_type=data_type)
-        functions_to_run = input("Enter the functions to run on this data: ")
+            my_data = Data(data_type=data_type)
+            my_data.read_data_file(data_file)
+
+        functions_to_run = input("Enter the functions to run on this data: ").split(", ")
         do_plot = input("Would you like to plot outputs? y/n:  ")
         do_save = input("Would you like to save the plots as png's? y/n:  ")
         for function_to_run in functions_to_run:
-            data = Analyzer.run_function(function_to_run) # TODO: Add this logic over in Analyzer
+            output = Analyzer.run_function(function_to_run, my_data.data) # TODO: Add this logic over in Analyzer
+            print("Function output: ", output)
             if do_plot:
-                plot_types = available_graphs[functions_to_run]
+                plot_types = available_graphs[function_to_run]
                 for plot_type in plot_types:
-                    visualize.plot_chart(data, plot_type, "<insert title>")
+                    visualize.plot_chart(output, plot_type, "<insert title>")
             continue
     return
 
