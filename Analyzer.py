825e7019331a8fbd045d20cfc05eef0016d224b2
McNichols <imcnichols@arete.com>
2022-01-31 17:21:51 -0600

    Adding more maths

--- Analyzer.py
+++ Analyzer.py
@@ -8,20 +8,26 @@ def run_mean(data):
     output = np.mean(data)
     return output
 
-def run_median(data): 
+
+def run_median(data):
     output = np.median(data)
     return output
+
 
 def run_mode(data):
     output = np.mode(data)
     return output
 
+
 def run_stand_dev(data):
-    standard_deviation = statistics.stdev(data)
+    standard_deviation = np.std(data)
     return standard_deviation
 
+
 def run_variance(data):
-    return statistics.variance(data)
+    variance = np.var(data)
+    return variance
+
 
 def run_percentiles(data):
     # Gets the percentile of the data at 0-100 percent in steps of 10
@@ -32,39 +38,61 @@ def run_percentiles(data):
         percentile_data.append(np.percentile(data, i))
     return percentile_data
 
+
 def run_probability_dist(data):
+    if len(data.shape) > 1:
+        print("Warning: Cannot run on 2D data. Using first dimension")
+    data = data[:,0]
     x = np.linspace(min(data), max(data), len(data))
     mu, std = stats.norm.fit(data)
     snd = stats.norm(mu, std)
     return snd.pdf(x), mu, std
 
+
 def run_binomial_dist(data):
     # TODO: Add logic
-    return
+    print('data shape:', data.shape)
+    p = len(np.where(data[:,0] < data[:,1])) / data.shape[0]
+    data_binom = stats.binom.rvs(n=data.shape[0], p=p)
+    return data_binom
+
 
 def run_chi_squared(data):
-    return stats.chi2_contingency(data[0], data[1])
+    if len(data.shape) < 2:
+        raise Exception("Data shape not accepted {}".format(str(data.shape)))
+    chi_squared_test = 0
+    for i in range(data.shape[0]):
+        chi_squared_test += (data[i][1] - data[i][0])**2 / data[i][0]
+    return chi_squared_test
+
 
 def run_least_square_line(data):
-    data_a = data[0]
-    data_b = data[1]
+    if len(data.shape) < 2:
+        raise Exception("Data shape not accepted {}".format(str(data.shape)))
+    data_a = data[:,0]
+    data_b = data[:,1]
     A = np.vstack([data_a, np.ones(len(data_a))]).T
     data_b = data_b[:, np.newaxis]
     alpha = np.dot((np.dot(np.linalg.inv(np.dot(A.T,A)),A.T)),data_b)
     line = data_a, alpha[0]*data_a + alpha[1]
-    return alpha
+    return line
+
 
 def run_correlation_coeff(data):
     return stats.pearsonr(data[0], data[1])
 
+
 def run_rank_sum(data):
     return stats.ranksums(data)
+
 
 def run_spearman_rank_corr_coeff(data):
     return stats.spearmanr(data[0], data[1])
 
 
 def run_function(function_name, data):
+    if len(data.shape) > 2:
+        raise Exception("Cannot handle data shape {}".format(str(data.shape)))
     if function_name == "mean":
         return run_mean(data)
     elif function_name == "median":
@@ -97,11 +125,16 @@ def run_function(function_name, data):
 
 if __name__ == "__main__":
     import Data
+    import visualize
 
-    my_data = Data.Data("Test_Data/IntervalDataTest.csv", "Interval")
-    my_data.read_data()
-    print("my data expected: ", my_data.pretest)
-    #print(run_mean(my_data.expected))
-    #print(run_stand_dev(my_data.expected))
-    #print(run_percentiles(my_data.expected))
-    print(run_chi_squared([my_data.pretest, my_data.posttest]))
+    my_data = Data.Data("Interval")
+    my_data.read_data_file("Data/IntervalDataTest.csv")
+    print("my data expected: ", my_data.data)
+    print('standard dev:', run_stand_dev(my_data.data))
+    print('variance:', run_variance(my_data.data))
+    print('probability dist:', run_probability_dist(my_data.data))
+    print('binomial dist:', run_binomial_dist(my_data.data))
+    print('least square:', run_least_square_line(my_data.data))
+    print('chi squared:', run_chi_squared(my_data.data))
+    line = run_least_square_line(my_data.data)
+    visualize.plot_chart([(line, 'line'), (my_data.data, 'dot')], plot_type='XY chart', title='least square regression')
