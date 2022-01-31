3e801b87d75dbfbd6c1078160c5fdf905a0fbfeb
McNichols <imcnichols@arete.com>
2022-01-31 17:22:08 -0600

    option to plot multiple things on the same graph

--- visualize.py
+++ visualize.py
@@ -15,12 +15,13 @@ def plot_chart(data, plot_type, title):
     elif plot_type == "normal distribution curve":
         print("plotting normal distribution curve")
     elif plot_type == "XY chart":
-        display_data = np.array(data)
-        if display_data.shape[0] != 2:
-            raise Exception("Invalid shape for XY chart data {}".format(display_data.shape))
         plt.figure()
+        for data_, plot_type in data:
+            if plot_type == 'line':
+                plt.plot(data_[0], data_[1], 'r')
+            elif plot_type == 'dot':
+                plt.plot(data_[:,0], data_[:,1], 'b.')
         plt.title(title)
-        plt.plot(display_data[0], display_data[1])
         plt.show()
     else:
         raise Exception("Invalid chart type {}".format(plot_type))
